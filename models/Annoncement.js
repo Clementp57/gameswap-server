f97c0686c48aa7d93a116e1ad73963ab4c36bf89
Mzkr <alexissourbet@gmail.com>
2015-10-21 01:14:53 +0200

    model annoncement

--- models/Annoncement.js
+++ models/Annoncement.js
@@ -3,10 +3,13 @@ var mongoose = require('mongoose');
 var Annoncements = new mongoose.Schema({
         id: String,
         title: String,
+        img : String,
         creator: String,
+        plateform : String,
         game: String,
         descript: String,
-        position: String
+        position: String,
+        date : String
     })
 
 module.exports = mongoose.model('Annoncement', Annoncements);
