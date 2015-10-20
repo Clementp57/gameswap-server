d4468fac7b7aa4d603fd45e53c39fbec5b5d9058
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-10-20 20:21:31 +0200

    removed secret file

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var Users = new mongoose.Schema({
-        id: String,
-        firstName: String,
-        lastName: String
-    });
+    id: String,
+    firstName: String,
+    lastName: String
+});
 
 module.exports = mongoose.model('User', Users);
