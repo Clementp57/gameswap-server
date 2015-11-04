bca8505900c11ec97fc19524640bf479c7fcf944
Clément Peyrabere <Clement@MacBook-Pro-de-Clement.local>
2015-11-04 22:05:15 +0100

    modified comments

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,10 @@ var mongoose = require('mongoose');
 var Comment = new mongoose.Schema({
         id: String,
         authorId: String,
+        authorName: {
+        	first: String,
+        	last: String
+        },
         content: String,
         date: Date,
         eventId: String,
